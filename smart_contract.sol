20955ae91588b50a378311120de5fd7b8329ff3d
Bhavesh Mishra <130285034+bhavesh0907@users.noreply.github.com>
2025-02-11 02:37:27 +0530

    smart_contract.sol

---
+++ smart_contract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+contract Voting {
+    mapping(string => uint256) public votes;
+    string[] public candidates;
+    
+    function addCandidate(string memory name) public {
+        candidates.push(name);
+        votes[name] = 0;
+    }
+    
+    function vote(string memory name) public {
+        require(votes[name] >= 0, "Candidate does not exist");
+        votes[name]++;
+    }
+    
+    function getVotes(string memory name) public view returns (uint256) {
+        return votes[name];
+    }
+}
